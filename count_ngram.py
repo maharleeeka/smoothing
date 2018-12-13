ae95aa5346a4243bc4f1d2514690007ef588448b
Maharlika Alberca <maharlikaalberca@gmail.com>
2018-12-13 22:10:44 +0800

    add-k

--- count_ngram.py
+++ count_ngram.py
@@ -2,27 +2,30 @@ import re
 from collections import Counter, defaultdict
 from itertools import islice
 
-word_array = [
-	"S i am a human",
-	"S i am not a stone",
-	"S i i live in lahone"
-];
+path = 'test_data.txt'
+
+test_data_file = open(path, 'r')
+word_array = test_data_file.readlines();
 
 i = 0;
 new_dictionary = Counter(zip('', islice('', 1, None)))
 word_frequency = Counter('');
 
 while i < len(word_array):
-	words = re.findall("\w+", word_array[i])
+	words = word_array[i]
+	words = words.lower()
+	words = words.replace('<s>', 'S')
+	words = words.replace('</s>', '')
+	words = re.findall("\w+", words)
 	dictionary = Counter(zip(words, islice(words, 1, None)))
 	word_frequency = word_frequency + Counter(words)
 	new_dictionary = new_dictionary + dictionary
 	i = i+1;
 
-print (new_dictionary) 
+# print (new_dictionary) 
 #new_dictionary - make a dictionary { bigram: count } i.e {('S', 'i'): 3, ... }
 
-print (word_frequency)  
+# print (word_frequency)  
 #word_frequency - make a dictionary { word: count } i.r {'S': 3, 'i': 4 }
 
 user_input = input("probability of: ")
@@ -35,7 +38,7 @@ user_input_dictionary = Counter(zip(user_input, islice(user_input, 1, None)))
 
 # looking_for_this_prob = user_input_dictionary.get(user_input[0], user_input[1])
 
-k_value = input("k value: ")
+k_value = float(input("k value: "))
 
 j = 0
 probability = 1
@@ -50,8 +53,13 @@ for key in user_input_dictionary:
 	else:
 		bigram_count = bigram_count + float(k_value)
 		
-	prob_each = bigram_count / (count + len(new_dictionary))
+	if (k_value > 0):
+		prob_each = bigram_count / (count + len(new_dictionary))
+	else:
+		prob_each = bigram_count / count
+
 	probability = probability * prob_each
 	j = j + 1
 
+print ('len: ', len(new_dictionary))
 print (probability)
